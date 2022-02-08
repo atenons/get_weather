d33f1d2c2058f4abeb0e8d867aa951007acb9288
Vladimir Semenov <atenon@gmail.com>
2022-02-08 12:38:37 +0300

    add description

--- weather.py
+++ weather.py
@@ -10,6 +10,7 @@ GEO_URL = f"http://api.openweathermap.org/geo/1.0/direct?"
 
 
 def _get_api_key():
+    """Fetch api-key from api.ini file"""
     config = ConfigParser()
     config.read("api.ini")
     return config["openweather"]["api_key"]
@@ -21,14 +22,12 @@ API_KEY = _get_api_key()
 def get_json(url, params):
     try:
         resp = httpx.get(url, params=params)
-        resp.raise_for_status()
     except httpx.HTTPError as exc:
         sys.exit(f"Error while requesting {exc.request.url!r}")
     return resp.json()
 
 
 def get_weather(location, limit, lang, api_key=API_KEY, units="metric"):
-
     params = {"q": location, "limit": limit, "appid": api_key}
     cities = get_json(GEO_URL, params=params)
     weather_in_cities = []
@@ -53,7 +52,8 @@ def get_weather(location, limit, lang, api_key=API_KEY, units="metric"):
 )
 @click.option("--lang", "-l", default="RU", help="use your preffered language")
 def main(location, amount_of_locations, lang):
-    """weather tool that shows you the current weather in locations of your choice"""
+    """Weather tool that shows you the current weather in locations of your choice.
+    API-key for connect to OpenWeatherAPI required"""
     weather = get_weather(location, amount_of_locations, lang)
     for loc in weather:
         print(
